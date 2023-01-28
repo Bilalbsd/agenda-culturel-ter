0b196ae8f3bb780a0eba68cd03857e5753c91c06
Bilalbsd <dev.bilalb@gmail.com>
2023-01-28 11:47:04 +0100

    Adding backend part in server/ and adding connection to mongodb cluster and server listening on index.js

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '.env' })
+
+main().then(() => console.log('Connected to MongoDB'))
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.set('strictQuery', true)
+    await mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.msot2us.mongodb.net/agenda-culturel');
+}
