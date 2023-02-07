afed16395fd52bd36fb29957b6ccae00aad819dd
Bilalbsd <dev.bilalb@gmail.com>
2023-02-07 16:07:28 +0100

    Add Header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+function Header() {
+  const { user, logout } = useAuth();
+
+  return (
+    <header>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/events">Events</Link>
+        {user ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
