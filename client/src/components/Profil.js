3c8940ef436dbda6d72ff638968fd8a1fbbe52df
Bilalbsd <dev.bilalb@gmail.com>
2023-02-27 15:33:41 +0100

    Updating Profil component

--- client/src/components/Profil.js
+++ client/src/components/Profil.js
@@ -2,61 +2,107 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import Login from './Login';
+import { Typography, TextField, Avatar, Box, Grid, Container, Button, Modal } from '@mui/material';
 
 function Profil() {
-    const { userId, isAuthenticated, userFirstname, userLastname, userEmail, userRole, title, companyName, address, phone } = useContext(AuthContext);
+    const { userId, isAuthenticated, userRole, userFirstname } = useContext(AuthContext);
 
     const [nbMaxEvent, setNbMaxEvent] = useState(null);
+    const [user, setUser] = useState({});
+
 
     useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const res = await axios.get(`http://localhost:5000/api/user/${userId}`);
-                setNbMaxEvent(res.data.nbMaxEvent); // mettre à jour la valeur de nbMaxEvent
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchUser();
-    }, [userId]); // déclencher l'effet à chaque changement de userId
+        axios
+            .get(`http://localhost:5000/api/user/${userId}`)
+            .then(res => { setUser(res.data); setNbMaxEvent(res.data.nbMaxEvent); console.log(user, "user") })
+            .catch(err => console.error(err));
+    }, [userId, nbMaxEvent]);
+
+    const handleChange = e => {
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios
+            .put(`http://localhost:5000/api/user/${userId}`, user)
+            .then(res => { console.log(res) })
+            .catch(err => console.error(err));
+    };
 
     return (
-        <div>
-            {isAuthenticated ? userRole === "creator" ?
-                <div>
-                    <div>
-                        <h1>Voici votre profil de créateur d'événement ! </h1>
-                        <h2>Id : {userId}</h2>
-                        <h2>Titre : {title}</h2>
-                        <h2>Nom de la société : {companyName}</h2>
-                        <h2>Adresse de la société : {address}</h2>
-                        <h2>Prénom : {userFirstname}</h2>
-                        <h2>Nom : {userLastname}</h2>
-                        <h2>Email : {userEmail}</h2>
-                        <h2>Numéro de téléphone : {phone}</h2>
-                        <h2>Nombre de création d'événement restant : {nbMaxEvent}</h2>
-                        <h2>Rôle : {userRole}</h2>
-                    </div>
-                </div>
-                :
-                <div>
-                    <div>
-                        <h1>Voici votre profil d'utilisateur ! </h1>
-                        <h2>Id : {userId}</h2>
-                        <h2>Prénom : {userFirstname}</h2>
-                        <h2>Nom : {userLastname}</h2>
-                        <h2>Email : {userEmail}</h2>
-                        <h2>Rôle : {userRole}</h2>
-                        <h2>Est connecté ? : {isAuthenticated ? "oui" : "non"}</h2>
-                    </div>
-                </div>
-                : <div>
-                    <p>Veuillez vous connecter pour voir votre profil</p> <Login />
-                </div>
-            }
-
-        </div>
-    );
+        <>
+            <Container maxWidth="sm">
+                {isAuthenticated ? (
+                    userRole === 'creator' ? (
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Box display="flex" justifyContent="center" alignItems="center">
+                                    <Avatar alt={user.firstname} src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100 }} variant="rounded" />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Prénom" name="firstname" value={"" + user.firstname} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Nom" name="lastname" value={"" + user.lastname} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Email" name="email" value={"" + user.email} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Numéro de téléphone" name="phone" value={"" + user.phone} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Titre" name="title" value={"" + user.title} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Nom de la société" name="companyName" value={"" + user.companyName} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Adresse de la société" name="address" value={"" + user.address} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField disabled label="Jetons de création" value={"" + nbMaxEvent} fullWidth />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField disabled label="Rôle" value={userRole} fullWidth />
+                            </Grid>
+                            <Grid item xs={12} >
+                                <Button value="Envoyer" onClick={handleSubmit}>Mettre à jour</Button>
+                            </Grid>
+                        </Grid>
+                    ) : (
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Box display="flex" justifyContent="center" alignItems="center">
+                                    <Avatar alt={userFirstname} src="/static/images/avatar/1.jpg" sx={{ width: 100, height: 100 }} variant="rounded" />
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Prénom" value={"" + user.firstname} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Nom" value={"" + user.lastname} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField label="Email" value={"" + user.email} fullWidth onChange={handleChange} />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField disabled label="Rôle" value={"" + userRole} fullWidth onChange={handleChange} />
+                            </Grid>
+                        </Grid>
+                    )
+                ) : (
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Typography variant="h6">Veuillez vous connecter pour voir   <Login /> </Typography>
+                        </Grid>
+                    </Grid>
+                )}
+            </Container>
+        </>
+    )
 };
 
 export default Profil;
--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Profil from '../components/Profil';
+import NavBar from '../components/NavBar';
 
 function ProfilePage() {
     return (
         <div>
-            <h2>Profil</h2>
+            <NavBar />
+            <br /> <br /> <br />
             <Profil />
         </div>
     );
