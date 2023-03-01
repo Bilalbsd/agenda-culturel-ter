8788d0dfd61570b6825d35cd23e2f03abf56c3c8
Bilalbsd <dev.bilalb@gmail.com>
2023-03-01 20:29:56 +0100

    Updating PersonalEvents component

--- client/src/pages/PersonalEvents.js
+++ client/src/pages/PersonalEvents.js
@@ -3,12 +3,14 @@ import EventDetail from '../components/EventDetail';
 import NavBar from '../components/NavBar';
 import EventsList from '../components/EventsList';
 import PersonalEvents from '../components/CreateEvent/PersonalEvents';
+import Footer from '../components/Footer';
 
 function Events() {
     return (
         <div>
             <NavBar />
             <PersonalEvents />
+            <Footer />
         </div>
     );
 }
