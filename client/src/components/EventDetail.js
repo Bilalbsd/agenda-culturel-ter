38f933e1d833921ff68131efbdfb335307e87303
Bilalbsd <dev.bilalb@gmail.com>
2023-04-22 18:50:41 +0200

    Updating EventDetail.js

--- client/src/components/EventDetail.js
+++ client/src/components/EventDetail.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 import moment from 'moment';
-import { Box, Grid, Typography, TextField, Rating, Button, IconButton, Avatar } from '@mui/material';
+import { Box, Grid, Typography, TextField, Rating, Button, IconButton, Avatar, List, Card, CardContent } from '@mui/material';
 import { Container } from '@mui/system';
 import 'moment/locale/fr';
 import { AuthContext } from '../context/AuthContext';
@@ -14,6 +14,20 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import UserGroupList from './UserGroupList';
 import FriendGroups from './FriendGroups';
 import FriendGroupsList from './FriendGroupsList';
+
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRange } from 'react-date-range';
+
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
 moment.locale('fr');
 
 function EventDetail() {
@@ -125,6 +139,46 @@ function EventDetail() {
         return () => clearInterval(intervalId);
     }, [id]);
 
+    const CLE_API = "AIzaSyBvGBV9DUig0t9hvtFy4YcTrouE8S22lQM";
+
+    const [initialCoords, setInitialCoords] = useState(null);
+    const [map, setMap] = useState(null);
+
+    useEffect(() => {
+        if (event.lat && event.lng) {
+            setInitialCoords([event.lat, event.lng]);
+        }
+    }, [event.lat, event.lng]);
+
+    console.log(initialCoords, "initialCoords")
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${CLE_API}&libraries=places`;
+        script.onload = () => {
+            const map = new window.google.maps.Map(document.getElementById("map"), {
+                center: { lat: initialCoords[0], lng: initialCoords[1] },
+                zoom: 15,
+            });
+            if (map && initialCoords) {
+                new window.google.maps.Marker({
+                    position: { lat: initialCoords[0], lng: initialCoords[1] },
+                    map: map,
+                });
+            }
+
+            setMap(map);
+        };
+        document.body.appendChild(script);
+    }, [initialCoords]);
+
+    const [state, setState] = useState([
+        {
+            startDate: moment(event.startDate).toDate(),
+            endDate: moment(event.endDate).toDate(),
+            key: 'selection'
+        }
+    ]);
 
     // console.log(moment(event.promotionExpirationDate).fromNow(), "event.promotionExpirationDate");
 
@@ -148,7 +202,22 @@ function EventDetail() {
                     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                         <img src={event.image} alt="img de l'évènement" style={{ width: "800px", height: "500px" }} />
                     </Box>
-                    <Typography variant="h3" align="left" color="text.primary" component="p">
+                    <Grid container spacing={2} justifyContent="center">
+                        <Grid item>
+                            {isAuthenticated ? <FavButton /> : <FavButton2 event={event} />}
+                        </Grid>
+                        <Grid item>
+                            {isAuthenticated ? <AgendaButton /> : <AgendaButton2 event={event} />}
+                        </Grid>
+                    </Grid>
+
+                    <Typography
+                        component="h3"
+                        variant="h3"
+                        align="center"
+                        color="text.primary"
+                        gutterBottom
+                    >
                         Informations
                     </Typography>
                     <Grid>
@@ -158,10 +227,30 @@ function EventDetail() {
                         <Grid item xs={12} md={6}>
                             <Typography variant="h5" component="h5">Ville: {event.city}</Typography>
                         </Grid>
+                        <Typography variant="h5" component="h5">Adresse: {event.location}</Typography>
+                        <Grid item xs={12}>
+                            <div id="map" style={{ height: "400px", width: "100%" }}></div>
+                        </Grid>
                         <Typography variant="h5" component="h5">Theme: {event.theme}</Typography>
                         <Typography variant="h5" component="h5">Date début: {moment(event.startDate).format('lll')}</Typography>
                         <Typography variant="h5" component="h5">Date fin: {moment(event.endDate).format('lll')}</Typography>
-                        <Typography variant="h5" component="h5">Adresse: {event.location}</Typography>
+
+                        {/* <h1>{event.title}</h1>
+                        {moment &&
+                            <DateRangePicker
+                                startDate={moment(event.startDate).format('MM/DD/YYYY')}
+                                endDate={moment(event.endDate).format('MM/DD/YYYY')}
+                                readOnly={true}
+                                readOnlyInputs={true}
+                            />
+                        } */}
+
+                        {/* <DateRange
+                            editableDateInputs={true}
+                            moveRangeOnFirstSelection={false}
+                            ranges={state}
+                        /> */}
+
                         {event.speaker && <Typography variant="h5" component="h5">Intervenants: {event.speaker}</Typography>}
                         {event.speakerPresentation && <Typography variant="h5" component="h5">Présentation des Intervenants: {event.speakerPresentation}</Typography>}
                         {event.capacity && <Typography variant="h5" component="h5">Capacité d'accueil: {event.capacity}</Typography>}
@@ -185,30 +274,34 @@ function EventDetail() {
                             </Typography>
                         )}
 
-                        {event.prices &&
-                            event.prices.map(price => (
-                                <>
-                                    <Typography variant="h5" component="h5">
-                                        Titre : {price.title} <br />
-                                    </Typography>
-                                    <Typography variant="h5" component="h5">
-                                        Prix : {event.inPromotion ? `${price.discountedPrice}€ (prix initial : ${price.price}€)` : price.price === 0 ? "Gratuit" : `${price.price}€`} <br />
-                                    </Typography>
-                                    <Typography variant="h5" component="h5">
-                                        Condition : {price.condition} <br /> <br />
-                                    </Typography>
-                                </>
-                            ))}
-
+                        {event.prices && (
+                            <div style={{ display: "flex", flexWrap: "wrap", flexDiretion: 'row', gap: '15px', justifyContent: 'center' }}>
+                                {event.prices.map(price => (
+                                    <Card key={price.title} style={{ padding: "10px", maxWidth: '250px', textAlign: 'center' }}>
+                                        <CardContent>
+                                            <Typography component="h3" variant="h3" color="text.primary">
+                                                {price.title}
+                                            </Typography>
+                                            <Typography component="h2" variant="h3" color="text.primary">
+                                                {event.inPromotion ? (
+                                                    <div>
+                                                        <span style={{ textDecoration: 'line-through', fontSize: '2rem' }}>{price.price}€</span> <span style={{ color: 'green' }}>{price.discountedPrice}€</span>
+                                                    </div>
+                                                ) : (
+                                                    price.price === 0 ? "Gratuit" : <spans style={{ color: 'green' }}>{price.price}€</spans>
+                                                )}
+                                            </Typography>
+                                            <Typography component="h3" variant="h5" color="text.secondary">
+                                                {price.condition}
+                                            </Typography>
+                                        </CardContent>
+                                    </Card>
+                                ))}
+                            </div>
+                        )}
 
                         <Typography variant="h5" component="h5">lien du site:<Link href={event.ticketLink}>{event.ticketLink}</Link></Typography>
                     </Grid>
-
-                    {isAuthenticated ? <FavButton /> : <FavButton2 event={event} />}
-                    {isAuthenticated ? <AgendaButton /> : <AgendaButton2 event={event} />}
-
-                    {/* <UserGroupList /> */}
-
 
                     <Box sx={{ width: '100%', mb: 5 }}>
                         {event.comments?.map(comment => (
